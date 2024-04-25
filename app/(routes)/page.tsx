021bb35b6bad6c0a1895e327dd6c24731400519f
Eddie Wong Chung Pheng <jeddiewong@yahoo.com>
2024-04-25 09:10:55 +0800

    update billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,20 +8,18 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("ae5e5b30-9ba2-486b-ad36-20935e40267a");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard 
-          data={billboard}
-        />
+        <Billboard data={billboard} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
       </div>
     </Container>
-  )
+  );
 };
 
 export default HomePage;
